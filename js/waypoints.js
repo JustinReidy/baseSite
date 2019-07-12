e6c25c2ef5016882131fffc65e3bf21569fffb02
Justin Reidy <justintreidy@gmail.com>
2019-07-12 00:29:10 -0500

    added second row of projects/changed button color, added form API and got it working

--- js/waypoints.js
+++ js/waypoints.js
@@ -3,6 +3,14 @@ var $badge = $('.about-site-waypoint');
 var $projects = $('.project-waypoint')
 var $contact = $('.contact-waypoint')
 
+$about.waypoint(function(direction) {
+    if (direction == 'down'){
+       $about.addClass('js-badge-animate');
+    } else {
+        $about.removeClass('js-badge-animate');
+    }
+
+}, { offset: '75%'});
 
 $badge.waypoint(function(direction) {
     if (direction == 'down'){
@@ -13,3 +21,20 @@ $badge.waypoint(function(direction) {
 
 }, { offset: '75%'});
 
+$projects.waypoint(function(direction) {
+    if (direction == 'down'){
+       $projects.addClass('js-badge-animate');
+    } else {
+        $projects.removeClass('js-badge-animate');
+    }
+
+}, { offset: '75%'});
+
+$contact.waypoint(function(direction) {
+    if (direction == 'down'){
+       $contact.addClass('js-badge-animate');
+    } else {
+        $contact.removeClass('js-badge-animate');
+    }
+
+}, { offset: '75%'});
